f3d28f3838be3a39975023571c8eb1cc4a8214cb
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2025-08-07 20:10:36 -0500

    Add Azure login workflow and Python script for Graph API interaction

---
+++ local_files/graph-api.py
@@ -0,0 +1,25 @@
+import os
+from azure.identity import ClientSecretCredential, AzureAuthorityHosts
+from kiota_authentication_azure.azure_identity_authentication_provider import AzureIdentityAuthenticationProvider
+from msgraph import GraphServiceClient, GraphRequestAdapter
+from msgraph_core import GraphClientFactory
+
+
+tenant_id     = os.getenv('AZURE_TENANT_ID', 'YOUR_TENANT_ID')
+client_id     = os.getenv('AZURE_CLIENT_ID', 'YOUR_CLIENT_ID')
+
+
+credential = ClientSecretCredential(
+    tenant_id=tenant_id,
+    client_id=client_id
+)
+
+# Use the .default scope for US Gov Graph:contentReference[oaicite:1]{index=1}
+scopes = ['https://graph.microsoft.com/.default']
+
+# Build the Kiota auth provider and disable CAE with is_cae_enabled=False:contentReference[oaicite:2]{index=2}
+client = GraphServiceClient(credentials=credential, scopes=scopes,)
+async def me():
+    me = await client.me.get()
+    if me:
+        print(me.display_name)
